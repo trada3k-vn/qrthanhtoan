ce87703bdee06b8ef9e1866ac1442fc2ee22abad
trada3k-vn <thaihai.3006@gmail.com>
2026-07-19 01:48:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const config = {
   qrImageUrl:
-    "https://plain-apac-prod-public.komododecks.com/202607/18/svY9Bbp82wieA7v6Pw9Y/image.jpg",
+    "https://plain-apac-prod-public.komododecks.com/202607/18/Lj4OeAv2xKrJZPXOhWcJ/image.jpg",
   transferNote: "Tên facebook của bạn (Không dấu)",
   brandTitle: "Thanh toán QR",
   downloadFileName: "ma-qr-thanh-toan.jpg"
